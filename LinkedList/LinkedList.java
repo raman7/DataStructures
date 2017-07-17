407f60b3891323bc4c8aa2302e9c0185662abedc
raman7 <sairaman63@yahoo.com>
2017-07-17 18:01:17 +0530

    LINKED LIST IMPLEMENTATION IN JAVA

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,41 @@
+class LinkedList{
+  static class Node{
+    int data;
+    Node prev;
+    Node next;
+  Node(int data){
+  this.data=data;
+  this.prev=null;
+  this.next=null;
+  }
+  }
+
+public static void main(String args[]){
+    //Creating a head pointer
+    Node head;
+    //create a Node
+    Node first = new Node(1);
+    head = first;
+    first.prev = head;
+    first.next=null;
+
+    //insert a second Node
+    Node second = new Node(2);
+    first.next=second;
+    second.prev = first;
+    second.next=null;
+//insert at the beginning of LinkedList
+Node third = new Node(10);
+third.next=head;
+head=third;
+third.prev = head;
+
+    //Searching a LinkedList for a key
+    Node temp = head;
+    while(temp!=null){
+      System.out.println(temp.data);
+      temp=temp.next;
+    }
+
+}
+}
